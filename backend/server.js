50ef1be003ffcb740eb9a4c1d5aaaa684f9bb270
luisalexandermm <alrxandermaturana76@gmail.com>
2026-04-15 21:13:33 -0500

    no funcioonan los cambios

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(helmet());
-app.use(express.json());
+app.use(express.json());  
 
 // Servir archivos estáticos (frontend)
 app.use(express.static(path.join(__dirname, '../frontend')));
